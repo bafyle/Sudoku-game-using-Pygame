1be4ad369f9a014ffa35839cffbdf5a7c99cca6b
Andrew <androw.emad@yahoo.com>
2022-11-12 23:50:36 +0200

    replaced the insertion loop with executemany method

--- database/create_database.py
+++ database/create_database.py
@@ -3,22 +3,23 @@ import sqlite3 as sql
 
 puzzles_number = 100 # first 100 puzzle
 
-connection = sql.connect("puzzles.db")
+connection = sql.connect(r"./puzzles.db")
+"""
+The database location is relative to the parent directory, not to this directory.
+"""
 cur = connection.cursor()
 
 # create the database schema
-with open("database/Table Creation.sql", 'r') as f:
+with open(r"database/Table Creation.sql", 'r') as f:
     cur.executescript(f.read())
 
-data = pd.read_csv("database/sudoku.csv")
+data = pd.read_csv("database/sudoku.csv") # relative location to parent dir
 
-for i in range(puzzles_number):
-    """
-    Inserting first 100 puzzles into the database, since the primary key is auto-incremented,
-    There is no need to specify it
-    """
-    cur.execute("INSERT INTO Quizzes(quiz) VALUES(?);", [data["quizzes"][i]])
-    cur.execute("INSERT INTO Answers VALUES(?, ?);", [i+1, data["solutions"][i]])
+quizzes_params = [(quiz, ) for quiz in data["quizzes"].values[:puzzles_number]]
+answers_params = [(index+1, answer, ) for index, answer in enumerate(data["solutions"].values[:puzzles_number])]
+
+cur.executemany("INSERT INTO Quizzes(quiz) VALUES(?);", quizzes_params)
+cur.executemany("INSERT INTO Answers VALUES(?, ?);", answers_params)
  
 connection.commit() # commit changes
 connection.close()
