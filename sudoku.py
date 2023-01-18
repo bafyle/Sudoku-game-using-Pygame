c7cde2cc5bbaf7d1aafdce61a376da70db6ff0e4
Andrew <androw.emad@yahoo.com>
2023-01-18 14:51:18 +0200

    Fixed a bug in numpad buttons and code refactoring

--- sudoku.py
+++ sudoku.py
@@ -34,7 +34,7 @@ class Game:
         self.buttons_list = [
             Buttons.Button("Check your answer", (550, 50, 200, 30), self.win),
             Buttons.Button("Show answer", (550, 150, 200, 30), self.win),
-            Buttons.Button("Clear", (550, 250, 200, 30), self.win),
+            Buttons.Button("Clear board", (550, 250, 200, 30), self.win),
             Buttons.Button("Next puzzle", (550, 450, 200, 30), self.win),
             Buttons.Button("Hint", (550, 350, 200, 30), self.win),
         ]
@@ -142,7 +142,7 @@ class Game:
         """
         self.board.puzzle = copy.deepcopy(self.board.original_puzzle)
         self.board.refresh_cells()
-        self.board.clear_selection()
+        self.board.deselect_cell()
         self.notification.invoke_notification("Board cleared", 1)
 
     def reset_hints(self):
@@ -178,97 +178,11 @@ class Game:
                     self.close_database()
                     self.game_running = False
 
-                # if the user clicked while the mouse is near the puzzle cells
-                # then search for the cell that he selected
-                # and save its row and column number
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        if pygame.mouse.get_pos() > (14, 14) and pygame.mouse.get_pos() <= (484, 484):
-                            for i, row in enumerate(self.board.cells):
-                                breakPoint = False
-                                for j, cell in enumerate(row):
-                                    if cell.rect.collidepoint(pygame.mouse.get_pos()) and cell.empty:
-                                        self.board.select_cell(i, j)
-                                        breakPoint = True
-                                        break
-                                if breakPoint:
-                                    break
-
-                        # if the user clicked in buttons area
-                        # execute the action function of the pressed button
-                        elif pygame.mouse.get_pos() > (540, 0):
-                            if self.buttons_list[0].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[0].Enable:
-                                valid_puzzle = self.check_valid_puzzle_action()
-
-                                if valid_puzzle:
-                                    self.board.solved = True
-                                    self.notification.invoke_notification("Your answer is correct!!", 0)
-                                else:
-                                    self.notification.invoke_notification("Think again", 0)
-                                
-                            elif self.buttons_list[1].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[1].Enable:
-                                self.show_answer_action()
-                                self.buttons_list[2].Enable = False # disable the reset button
-
-                            elif self.buttons_list[2].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[2].Enable:
-                                self.reset_board()
-
-                            elif self.buttons_list[3].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[3].Enable:
-                                self.get_next_puzzle_action()
-                                self.reset_game_timer()
-                                self.reset_hints()
-                                self.notification.kill_notification()
-                                self.buttons_list[2].Enable = True # enable the reset button
-                            
-                            elif self.buttons_list[4].rect.collidepoint(pygame.mouse.get_pos()):
-                                self.hint_action()
-                    
-                    # change volume using mouse wheel
-                    elif event.button == 4:
-                        if pygame.mixer.music.get_volume() != 1:
-                            pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.1)
-                    elif event.button == 5:
-                        if pygame.mixer.music.get_volume() <= 0.1:
-                            pygame.mixer.music.set_volume(0)
-                        else:
-                            pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() - 0.1)
+                    self._mouse_button_pressed_event_handling(event)
 
                 elif event.type == pygame.KEYDOWN:
-                    # inserting a number from the num-pad to an empty cell
-                    if pygame.K_KP0 <= event.key <= pygame.K_KP9 or pygame.K_0 <= event.key <= pygame.K_9:
-                        if self.board.position_of_selected_cell != ():
-                            r, c = self.board.position_of_selected_cell
-                            if self.board.cells[r][c].empty:
-                                if pygame.K_KP0 <= event.key <= pygame.K_KP9:
-                                    self.board.puzzle[r][c] = event.key + 1 - pygame.K_KP1
-                                elif pygame.K_0 <= event.key <= pygame.K_9:
-                                    self.board.puzzle[r][c] = event.key - pygame.K_0
-                                self.board.refresh_cells()
-                    
-                    # navigate the board using arrows keys
-                    elif pygame.K_UP >= event.key >= pygame.K_RIGHT:
-                        if self.board.selected_cell is not None:
-                            if event.key == pygame.K_UP:
-                                new_row = (self.board.position_of_selected_cell[0] - 1) % 9
-                                new_column = self.board.position_of_selected_cell[1]
-                            elif event.key == pygame.K_DOWN:
-                                new_row = (self.board.position_of_selected_cell[0] + 1) % 9
-                                new_column = self.board.position_of_selected_cell[1]
-                            elif event.key == pygame.K_LEFT:
-                                new_row = self.board.position_of_selected_cell[0]
-                                new_column = (self.board.position_of_selected_cell[1] - 1 ) % 9
-                            else:
-                                new_row = self.board.position_of_selected_cell[0]
-                                new_column = (self.board.position_of_selected_cell[1] + 1 ) % 9
-                            if self.board.cells[new_row][new_column].empty:
-                                self.board.select_cell(new_row, new_column)
-
-                    # go to the next empty cell using tab
-                    elif pygame.K_TAB == event.key:
-                        if self.board.selected_cell is not None:
-                            r, c = self.board.get_next_empty_cell()
-                            if r != -1:
-                                self.board.select_cell(r, c)
+                    self._keyboard_key_pressed_event_handling(event)
             
             # change the color of any button if the mouse is hovering over it
             if pygame.mouse.get_pos() > (540, 0):
@@ -278,10 +192,9 @@ class Game:
                     else:
                         button.on_mouse_exit()
 
-            self.win.fill(Game.BACKGROUND_COLOR) # black background
-
-
             #-------------drawing section-------------#
+
+            self.win.fill(Game.BACKGROUND_COLOR) # black background
 
             for rowOfCells in self.board.cells:
                 for cell in rowOfCells:
@@ -313,8 +226,113 @@ class Game:
             pygame.display.flip()
 
         sys.exit(0)
-                
 
+    def _mouse_button_pressed_event_handling(self, event):
+        if event.button == 1:
+            if pygame.mouse.get_pos() > (14, 14) and pygame.mouse.get_pos() <= (484, 484):
+                self._mouse_button_1_pressed_on_board(event)
+
+            # if the user clicked in buttons area
+            # execute the action function of the pressed button
+            elif pygame.mouse.get_pos() > (540, 0):
+                self._mouse_button_1_pressed_on_buttons(event)
+        
+        # change volume using mouse wheel
+        elif event.button == 4:
+            if pygame.mixer.music.get_volume() != 1:
+                pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.1)
+        elif event.button == 5:
+            if pygame.mixer.music.get_volume() <= 0.1:
+                pygame.mixer.music.set_volume(0)
+            else:
+                pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() - 0.1)
+    
+    def _keyboard_key_pressed_event_handling(self, event):
+        if (pygame.K_KP1 <= event.key <= pygame.K_KP9) or (pygame.K_1 <= event.key <= pygame.K_9):
+            self._keyboard_number_pressed(event)
+        
+        if pygame.K_UP >= event.key >= pygame.K_RIGHT:
+            self._keyboard_arrows_pressed(event)
+
+        if pygame.K_TAB == event.key:
+            self._keyboard_tab_pressed(event)
+
+    def _keyboard_number_pressed(self, event):
+        # inserting a number from the num-pad to an empty cell
+        if self.board.position_of_selected_cell == ():
+            return
+        r, c = self.board.position_of_selected_cell
+        if not self.board.cells[r][c].empty:
+            return
+        if pygame.K_KP1 <= event.key <= pygame.K_KP9:
+            self.board.puzzle[r][c] = event.key + 1 - pygame.K_KP1
+        elif pygame.K_1 <= event.key <= pygame.K_9:
+            self.board.puzzle[r][c] = event.key - pygame.K_0
+        self.board.refresh_cells()
+    
+    def _keyboard_arrows_pressed(self, event):
+        # navigate the board using arrows keys
+        if self.board.selected_cell is None:
+            return
+        if event.key == pygame.K_UP:
+            new_row = (self.board.position_of_selected_cell[0] - 1) % 9
+            new_column = self.board.position_of_selected_cell[1]
+        elif event.key == pygame.K_DOWN:
+            new_row = (self.board.position_of_selected_cell[0] + 1) % 9
+            new_column = self.board.position_of_selected_cell[1]
+        elif event.key == pygame.K_LEFT:
+            new_row = self.board.position_of_selected_cell[0]
+            new_column = (self.board.position_of_selected_cell[1] - 1 ) % 9
+        else:
+            new_row = self.board.position_of_selected_cell[0]
+            new_column = (self.board.position_of_selected_cell[1] + 1 ) % 9
+        if self.board.cells[new_row][new_column].empty:
+            self.board.select_cell(new_row, new_column)
+    
+    def _keyboard_tab_pressed(self, event):
+        # go to the next empty cell using tab
+        if self.board.selected_cell is None:
+            return
+        r, c = self.board.get_next_empty_cell()
+        if r != -1:
+            self.board.select_cell(r, c)
+    
+    def _mouse_button_1_pressed_on_board(self, event):
+        for i, row in enumerate(self.board.cells):
+            for j, cell in enumerate(row):
+                if cell.rect.collidepoint(pygame.mouse.get_pos()) and cell.empty:
+                    if self.board.selected_cell == cell:
+                        self.board.deselect_cell()
+                    else:
+                        self.board.select_cell(i, j)
+                    return
+    
+    def _mouse_button_1_pressed_on_buttons(self, event):
+        if self.buttons_list[0].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[0].Enable:
+            valid_puzzle = self.check_valid_puzzle_action()
+            if valid_puzzle:
+                self.board.solved = True
+                self.notification.invoke_notification("Your answer is correct!!", 0)
+            else:
+                self.notification.invoke_notification("Think again", 0)
+            
+        elif self.buttons_list[1].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[1].Enable:
+            self.show_answer_action()
+            self.buttons_list[2].Enable = False # disable the reset button
+
+        elif self.buttons_list[2].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[2].Enable:
+            self.reset_board()
+
+        elif self.buttons_list[3].rect.collidepoint(pygame.mouse.get_pos()) and self.buttons_list[3].Enable:
+            self.get_next_puzzle_action()
+            self.reset_game_timer()
+            self.reset_hints()
+            self.notification.kill_notification()
+            self.buttons_list[2].Enable = True # enable the reset button
+        
+        elif self.buttons_list[4].rect.collidepoint(pygame.mouse.get_pos()):
+            self.hint_action()
+                    
 
 def main():
     game = Game()
